e95c8c96a3b7772f6d8bc13f2ae4787caab4ce9a
yuki-325 <sakeikura325@yahoo.co.jp>
2020-02-24 16:20:26 +0900

    commit test

--- ChatApp/Controller/RegisterViewController.swift
+++ ChatApp/Controller/RegisterViewController.swift
@@ -7,24 +7,57 @@
 //
 
 import UIKit
+import Firebase
+import Lottie
 
 class RegisterViewController: UIViewController {
 
+    @IBOutlet weak var emailTextField: UITextField!
+    
+    @IBOutlet weak var passwordTextField: UITextField!
+    
+    let animationView = AnimationView()
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    //Firebaseにユーザを新規登録する
+    @IBAction func registerNewUser(_ sender: Any) {
+   
+        //アニメーションのスタート
+        
+        //新規登録処理
+        Auth.auth().createUser(withEmail: emailTextField.text!, password: passwordTextField.text!) { (user, error) in
+            
+            if error != nil {
+                print(error as Any)
+            } else {
+                print("ユーザの作成が成功しました")
+                
+                //アニメーションのストップ
+                
+                //画面をチャット画面に遷移させる
+                
+            }
+        }
     }
-    */
-
+    
+    func startAnimation() {
+        //アニメーションの種類を設定
+        let animation = Animation.named("Loading")
+        
+        //animationが行われる位置を指定する
+        animationView.frame = CGRect(x: 0, y: 0, width: view.frame.size.width, height: view.frame.size.height / 1.5)
+        
+        //設定した場所に任意のアニメーションを入れる
+        animationView.animation = animation
+        
+    }
+    
+    
 }
---
+++ ChatApp/Controller/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ChatApp
+//
+//  Created by 中野勇貴 on 2020/02/23.
+//  Copyright © 2020 Nakano Yuki. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
